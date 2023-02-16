2672b893783e90629171df4a0ff42ef75d31af0b
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-02-16 15:16:03 +0100

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/vite.svg'
-import './App.css'
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/login/Login";
+import Register from "./components/signup/signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [userstate, setUserState] = useState({});
   return (
     <div className="App">
-      hello
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              userstate && userstate._id ? (
+                <Profile
+                  setUserState={setUserState}
+                  username={userstate.fname}
+                />
+              ) : (
+                <Login setUserState={setUserState} />
+              )
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={<Login setUserState={setUserState} />}
+          ></Route>
+          <Route path="/signup" element={<Register />}></Route>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
